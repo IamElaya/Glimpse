c2f6e1a6c4167a4104a0ee0995741fa890bf449b
Elaya Sison <asison13@gmail.com>
2016-03-07 14:55:45 -0800

    Changed port

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ module.exports = {
    // Lets the app reload on the fly.
    inline: true,
    // Sets the host.
-   // host: '0.0.0.0',
+   host: '0.0.0.0',
    // // Specifies the port.
-   port: process.env.PORT || 5000
-   // watchOptions: {
-   //   aggregateTimeout: 300,
-   //   poll: 1000
-   // }
+   port: (process.env.PORT || 8000),
+   watchOptions: {
+     aggregateTimeout: 300,
+     poll: 1000
+   }
   },
   module: {
     loaders: [
